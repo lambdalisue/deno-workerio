a26398ef16591a933bda3c6edc444ce7c3a556a0
Alisue <lambdalisue@hashnote.net>
2021-06-13 20:54:16 +0900

    Add spec comment to explain issue #5
    Close #5

--- writer.ts
+++ writer.ts
@@ -7,6 +7,14 @@ export class WorkerWriter implements Deno.Writer {
     this.#worker = worker;
   }
 
+  /**
+   * Write data to the peer through Worker.postMessage
+   *
+   * Note that this method does NOT guarantee if the peer
+   * receive the data.
+   * See the below link for the detail.
+   * https://github.com/lambdalisue/deno-workerio/issues/5
+   */
   write(p: Uint8Array): Promise<number> {
     // XXX
     // Send 'p' as-is once the issue below has resolved.
