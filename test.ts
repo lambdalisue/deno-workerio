1fb8fb8e14b39fdd461c232bfc00985f5bdc0707
Alisue <lambdalisue@gmail.com>
2023-04-28 10:52:11 +0900

    Fix benchmark test

--- test.ts
+++ test.ts
@@ -14,132 +14,102 @@ async function timeIt(fn: () => Promise<void>): Promise<number> {
   return elapsed;
 }
 
-Deno.test({
-  name: "Transferring small data from Self to Worker to Self",
-  fn: async () => {
+Deno.test("benchmark test", async (t) => {
+  const runTest = (
+    size: number,
+  ): [Worker, () => Promise<void>, () => Promise<void>] => {
     const worker = new Worker(
       new URL("./test_worker.ts", import.meta.url).href,
       {
         type: "module",
       },
     );
-    try {
-      const reader = new WorkerReader(worker);
-      const writer = new WorkerWriter(worker);
+    const reader = new WorkerReader(worker);
+    const writer = new WorkerWriter(worker);
+    const buffer = new Uint8Array(size);
 
-      const size = 16;
-      const buffer = new Uint8Array(size);
+    const consumer = async () => {
+      const r = await streams.readAll(
+        new io.LimitedReader(reader, size),
+      );
+      assertEquals(r.length, size);
+    };
 
-      const consumer = async () => {
-        const r = await streams.readAll(
-          new io.LimitedReader(reader, size),
-        );
-        assertEquals(r.length, size);
-      };
+    const producer = async () => {
+      await streams.writeAll(writer, buffer);
+    };
 
-      const producer = async () => {
-        await streams.writeAll(writer, buffer);
-      };
+    return [worker, consumer, producer];
+  };
 
-      const [rt, wt] = await Promise.all([
-        timeIt(consumer),
-        timeIt(producer),
-      ]);
+  // Warming up (let JIT to compile code)
+  const [worker, consumer, producer] = runTest(1);
+  await Promise.all([consumer(), producer()]);
+  worker.terminate();
 
-      // Peformance check
-      const rtThreshold = 500;
-      const wtThreshold = 500;
-      assert(rt < rtThreshold, "Reader is too slow");
-      assert(wt < wtThreshold, "Writer is too slow");
-    } finally {
-      worker.terminate();
-    }
-  },
-  sanitizeOps: false,
-});
+  await t.step({
+    name: "Transferring small data from Self to Worker to Self",
+    fn: async () => {
+      const [worker, consumer, producer] = runTest(16);
+      try {
+        const [rt, wt] = await Promise.all([
+          timeIt(consumer),
+          timeIt(producer),
+        ]);
 
-Deno.test({
-  name: "Transferring medium data from Self to Worker to Self",
-  fn: async () => {
-    const worker = new Worker(
-      new URL("./test_worker.ts", import.meta.url).href,
-      {
-        type: "module",
-      },
-    );
-    try {
-      const reader = new WorkerReader(worker);
-      const writer = new WorkerWriter(worker);
+        // Peformance check
+        const rtThreshold = 500;
+        const wtThreshold = 500;
+        assert(rt < rtThreshold, "Reader is too slow");
+        assert(wt < wtThreshold, "Writer is too slow");
+      } finally {
+        worker.terminate();
+      }
+    },
+    sanitizeOps: false,
+  });
 
-      const size = 1024 * 32;
-      const buffer = new Uint8Array(size);
+  await t.step({
+    name: "Transferring medium data from Self to Worker to Self",
+    fn: async () => {
+      const [worker, consumer, producer] = runTest(1024 * 32);
+      try {
+        const [rt, wt] = await Promise.all([
+          timeIt(consumer),
+          timeIt(producer),
+        ]);
 
-      const consumer = async () => {
-        const r = await streams.readAll(
-          new io.LimitedReader(reader, size),
-        );
-        assertEquals(r.length, size);
-      };
+        // Peformance check
+        const rtThreshold = 750;
+        const wtThreshold = 750;
+        assert(rt < rtThreshold, "Reader is too slow");
+        assert(wt < wtThreshold, "Writer is too slow");
+      } finally {
+        worker.terminate();
+      }
+    },
+    sanitizeOps: false,
+  });
 
-      const producer = async () => {
-        await streams.writeAll(writer, buffer);
-      };
+  await t.step({
+    name: "Transferring massive data from Self to Worker to Self",
+    fn: async () => {
+      const [worker, consumer, producer] = runTest(1024 * 1024 * 32);
+      try {
+        const [rt, wt] = await Promise.all([
+          timeIt(consumer),
+          timeIt(producer),
+        ]);
 
-      const [rt, wt] = await Promise.all([
-        timeIt(consumer),
-        timeIt(producer),
-      ]);
-
-      // Peformance check
-      const rtThreshold = 750;
-      const wtThreshold = 750;
-      assert(rt < rtThreshold, "Reader is too slow");
-      assert(wt < wtThreshold, "Writer is too slow");
-    } finally {
-      worker.terminate();
-    }
-  },
-  sanitizeOps: false,
-});
-
-Deno.test({
-  name: "Transferring massive data from Self to Worker to Self",
-  fn: async () => {
-    const worker = new Worker(
-      new URL("./test_worker.ts", import.meta.url).href,
-      {
-        type: "module",
-      },
-    );
-    try {
-      const reader = new WorkerReader(worker);
-      const writer = new WorkerWriter(worker);
-
-      const size = 1024 * 1024 * 32;
-      const buffer = new Uint8Array(size);
-
-      const consumer = async () => {
-        const r = await streams.readAll(new io.LimitedReader(reader, size));
-        assertEquals(r.length, size);
-      };
-
-      const producer = async () => {
-        await streams.writeAll(writer, buffer);
-      };
-
-      const [rt, wt] = await Promise.all([
-        timeIt(consumer),
-        timeIt(producer),
-      ]);
-
-      // Peformance check
-      const rtThreshold = 1000;
-      const wtThreshold = 1000;
-      assert(rt < rtThreshold, "Reader is too slow");
-      assert(wt < wtThreshold, "Writer is too slow");
-    } finally {
-      worker.terminate();
-    }
-  },
-  sanitizeOps: false,
+        // Peformance check
+        const rtThreshold = 1000;
+        const wtThreshold = 1000;
+        assert(rt < rtThreshold, "Reader is too slow");
+        assert(wt < wtThreshold, "Writer is too slow");
+      } finally {
+        worker.terminate();
+      }
+    },
+    sanitizeOps: false,
+  });
 });
